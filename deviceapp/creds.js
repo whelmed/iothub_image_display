005f85e9605e34cfb43c65f24431a2885488f55b
Ben Lambert <ben.lambert@cloudacademy.com>
2017-05-03 14:11:50 -0400

    update creds.js

---
+++ deviceapp/creds.js
@@ -0,0 +1,6 @@
+// Change as needed. These creds are just placeholders.
+module.exports = {
+    HostName        : "ca-iot-hub-solution.azure-devices.net",
+    SharedAccessKey : "YOUR KEY HERE",
+    DeviceId        : "YOUR DEVICE ID HERE",
+};
