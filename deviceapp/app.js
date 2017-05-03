653bfd8cbc2849b6146be4b981ebddf27c259bcc
Ben Lambert <ben.lambert@cloudacademy.com>
2017-05-03 12:59:36 -0400

    testing code to show the image

--- deviceapp/app.js
+++ deviceapp/app.js
@@ -4,9 +4,10 @@ var Protocol    = require('azure-iot-device-mqtt').Mqtt,
     Client      = require('azure-iot-device').Client,
     Message     = require('azure-iot-device').Message,
     creds       = require('./creds.js'),
+    os          = require('os'),
     client      = null;
-    
-    
+
+const exec = require('child_process').exec;    
 
 function main() {
 
@@ -41,8 +42,38 @@ function connect(err) {
 
     // Listen for IoT Hub sending messages
     client.on('message', function(msg) {
+        if (os.platform() === 'linux') {
+            download(msg.imageUrl, function() {
+                // Kill any image viewers
+                var file = '/tmp/' + getFileName(uri);
+                exec(`pkill fbi -2 && fbi -T 2 ${file}`, (error, stdout, stderr) => {
+                    if (error) {
+                        console.error(`exec error: ${error}`);
+                        return;
+                    }
+                    
+                });
+            });
+        }
         console.log('Feedback message:')
         console.log(msg.getData().toString('utf-8'));
     });
 }
+
+function getFileName(uri) {
+    var urlSections = uri.split('/');
+    return urlSections[urlSections-1];
+}
+
+function download(uri, callback) {
+  console.log("Downloading new image from: ", uri);
+  
+  request.head(uri, function(err, res, body) {
+      
+    var r = request(uri).pipe(fs.createWriteStream('/tmp/' + getFileName(uri)));
+    r.on('close', callback);
+    r.on('error', error);
+  });
+}
+
 main();
